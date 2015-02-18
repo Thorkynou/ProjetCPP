b74f463a7cfaf289f26ae57084d18475e44b3678
VERNIER <benedicte.vernier@gmail.com>
2015-02-18 16:34:27 +0100

    comptecourant modifie et creation des fichiers date. le date.cpp va etre completer

--- comptecourant.h
+++ comptecourant.h
@@ -1,9 +1,11 @@
-/*creation du fichier compte courant.h
+/*creation du fichier comptecourant.h
 fihcier realise par benedicte le */
 
 #include <iostream>
 #include <string>
+using namespace std;
 
+#include "date.h"
 
 #ifndef COMPTECOURANT_H
 #define COMPTECOURANT_H
@@ -13,15 +15,18 @@ class comptecourant
 
 private:
         long numerodecompte;//numero du compte courant
-        int solde;//solde du compte
-        int decouvert;//variable autorisant un decouvert au client
+        double solde;//solde du compte
+        double decouvert;//variable autorisant un decouvert au client
         double taux;//taux du compte courant=0.30%
+        bool debiteur;//verification du solde<0 pour calculer les agios
+
 public:
-        int agios;//calcul des agios si on depasse le decouvert
+        double montantdebiteur;//calcule le montantdebietur a chaque fois que le bool debiteur est true on incremente apres a chaque passage
+        double agios;//calcul des agios si on depasse le decouvert
 
         //comptecourant();//constructeur par defaut
-        comptecourant(long numerodecompte,int solde=0,int decouvert=0,double taux=0.30);//constructeur par parametres par defaut
-        comptecourant(const comptecourant &C);//constructeur par copie
+        comptecourant(long numerodecompte,double solde=0,double decouvert=0,double taux=0.30);//constructeur par parametres par defaut
+        comptecourant(const comptecourant &CC);//constructeur par copie
 
         void Retirer();//retrait d'argent sur le compte
         void Ajouter();//ajout d'argent sur le compte
@@ -31,19 +36,29 @@ public:
         void AfficherSolde(ostream & out);//affiche le solde du compte
         void AffichageAlerte(ostream & out);//affichage d'un message d'alerte quand le solde =le decouvert autorise
 
-        int CalculAgios();//calcul des agios (voir internet)
-        void AffichageAgios()const;//affichage des agios que le client doit payer
+        int CalculAgios(double solde,bool debiteur);//calcul des agios
+        //etape 1 : montantdebiteur=nbjdebiteur*debit //pour chaque montant faire le calcul
+        //etape2: agios=(montantdebiteur*txinteretdecouvert)/(365*100)
+
+        /*compte debiteur du 5 au 22 janvier de 500 euros et du 2 au 28 mars de 450.
+        montantdebiteur=(17*500)+(26*450)=20200
+        agios=(20200*12)/36500//tx d'interet de 12%
+        agios=11.07euros +5.90€ de taxe de commissions
+        */
+        void AffichageAgios(const double agios)const;//affichage des agios que le client doit payer
 
        void Afficher10Actions()const;//affichage des 10 dernieres actions faite sur le compte
+       //ouvrir un fichier et le fermer apres;
+       //recuperer avec un vector <comptecourant>les 10 dernieres lignes du fichiers , faire une boucle avec begin et end.
 
         ~CompteCourant();// destructeur du compte courant
 
 };
 
-comptecourant operator+(intN,const comptecourant &C);//pour l'ajout d'argent on aura solde3=operator+(100,solde2)  (solde3=100+solde2)
-comptecourant operator-(intN,const comptecourant &C);//pour le retrait d'argent on aura solde3=operator-(100,solde2)  (solde3=100-solde2)
+comptecourant operator+(int N,const comptecourant & CC);//pour l'ajout d'argent on aura solde3=operator+(100,solde2)  (solde3=100+solde2)
+comptecourant operator-(int N,const comptecourant & CC);//pour le retrait d'argent on aura solde3=operator-(100,solde2)  (solde3=100-solde2)
 
-istream & operator >>(istream &in,const comptecourant &C);
-ostream & operator<<(ostream &out,const comptecourant &C);
+istream & operator >>(istream &in,const comptecourant & CC);
+ostream & operator<<(ostream &out,const comptecourant & CC);
 
 #endif
---
+++ date.cpp
@@ -0,0 +1,12 @@
+//creation du fichier date.cpp
+//realise par benedicte
+
+#include "date.h"
+
+date::date()// constructeur par defaut
+
+void date::Saisir()//saisie de la date du jour
+void date::Afficher()//affiche la date du jour
+
+date::~date()//detsructeur par defaut
+
