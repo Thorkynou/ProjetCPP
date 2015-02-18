10ab40af3cdd9695feb8425449088beb23758368
Thorkynou <clement.laforgue@gmail.com>
2015-02-18 16:42:14 +0100

    Modif client.h et .cpp

--- client.h
+++ client.h
@@ -1,31 +1,44 @@
-/* client */
-
-#ifndef CLIENT_H
-#define CLIENT_H
+/**************************************************
+ *                  Projet C++                    *           
+ * Bénédicte, Marc, Alain, Clément S et Clément L *
+ *                                                *
+ * Fichier: client.h                              *
+ * @author: Clément L                             *
+ **************************************************/
 
 #include <iostream>
 #include <string>
-#include "comptecourant.h"
+//#include "comptecourant.h"
 
+#ifndef CLIENT_H
+#define CLIENT_H
 using namespace std;
 
 class Client
 {
 private:
-	nom;
-	prenom;
-	adresse;
+	//CompteCourant compteC;
+	string nom;
+	string prenom;
+	string adresse;
 	bool CC;
 	bool LE;
 	bool CB;
 	bool PEL;
 	bool EC;
 public: 
-	Client(string nom, string prenom, string adresse, bool CC = true, bool LE = false, bool CB = false, bool PEL = false, bool EC = false);
+	// Constructeur par paramètres
+	Client(string nom = "vide", string prenom = "vide", string adresse = "vide", bool CC = false, bool LE = false, bool CB = false, bool PEL = false, bool EC = false);
 
+	// Constructeur par copie
 	Client(const Client &C);
 
-	~Client(){};
+	//Destructeur
+	virtual ~Client(){};
+
+	virtual void Afficher(ostream &out) const;
+
+	virtual void ModifierAdresse(string adresse);
 };
 
 
