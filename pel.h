0f820ebbb14e4a254ed7b88b1ae3f10edbfa9f78
VERNIER <benedicte.vernier@gmail.com>
2015-02-18 11:12:27 +0100

    compte cc

---
+++ pel.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <strind>
+
+using namespace std;
+
+#ifndef PEL_H
+#define PEL_H
+
+class Pel, public GeneraliteCompte
+{
+private:
+    double versementMensuel;
+
+
+
+
+
+
+};
+
