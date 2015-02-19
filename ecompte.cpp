9d729f6f404dee360fd21e6cad6baff71a56549d
stag <stag@ldnr-formation>
2015-02-19 16:14:27 +0100

    ajout fonction

--- ecompte.cpp
+++ ecompte.cpp
@@ -1,26 +1,49 @@
 #include "ecompte.h"
 
-ecompte::ecompte(string eml ,string nt ,double tm,double t)
+ecompte::ecompte(string eml ,string nt ,double s,double tm,double t)
 {
-    email=eml;
-    numTel=nt;
-    taux3Mois=tm;
-    taux=t;
-    cout << "création ecompte";
+	email=eml;
+	numTel=nt;
+	solde=s;
+	taux3Mois=tm;
+	taux=t;
+	cout <<endl<<"*******************"<<endl<< "création ecompte"<<endl<<"*******************";
 }
 ecompte::ecompte(const ecompte & E)
 {
-    email=E.email;
-    numTel=E.numTel;
-    taux3Mois=E.taux3Mois;
-    taux=E.taux;
+	email=E.email;
+	numTel=E.numTel;
+	solde=E.solde;
+	taux3Mois=E.taux3Mois;
+	taux=E.taux;
 }
 ecompte &ecompte::CreerCompte()
 {
-cout<<endl<<"Saisir votre adresse email: ";
-cin >>this->email;
-cout<<endl<<"Saisir votre numero de telephone: ";
-cin >>this->numTel;
-return *this;
+	cout<<endl<<"Saisir votre adresse email: ";
+	cin >>this->email;
+	cout<<endl<<"Saisir votre numero de telephone: ";
+	cin >>this->numTel;
+	return *this;
 }
 ecompte :: ~ecompte() {}
+
+void ecompte::AfficherSolde(ostream & out)
+{
+	out<<endl<<"votre solde est actellement de :"<<solde<<"€"<<endl;
+}
+
+void ecompte::Retrait()
+{
+	int x=0;
+	while (x<10)
+	{
+		cout<<endl<<"Saisir la somme a retirer du ecompte :";
+		cin>>x;
+		if (x<10)
+			cout<<endl<<"somme minimale pour un retrait : 10 €";
+	}
+	solde=solde-x;
+	AfficherSolde(cout);
+	//cout<<"le nouveau solde du compte est \t"<<solde<<endl;
+
+    }
--- ecompte.h
+++ ecompte.h
@@ -1,3 +1,12 @@
+/*
+00888880080000008888800880880088888008800008008888888000008888800 
+00800000080000008000000808080080000008080008000008000000008000000 
+00800000080000008880000800080088880008008008000008000000008888800 
+00800000080000008000000800080080000008000808000008000000000000800 
+00888880088888008888800800080088888008000088000008000008008888800 
+
+Ctrl+f , 8 , enter                 
+*/
 #include <iostream>
 #include <string>
 using namespace std;
@@ -8,19 +17,22 @@ using namespace std;
 class ecompte //, public GeneraliteCompte
 {
 private:
-    string email; //si temps vérif @ présent et (dot) présent aussi
-    string numTel; //penser a vérifier que le l'utilisateur a saisit 10 chiffres pas plus/moins (voire si temps commence 0)
-    double taux3Mois;//taux pour les 3 premiers mois du compte
-    double taux;//taux pour les mois suivant les 3 premiers mois
+	string email; //si temps vérif @ présent et (dot) présent aussi
+	string numTel; //penser a vérifier que le l'utilisateur a saisit 10 chiffres pas plus/moins (voire si temps commence 0)
+	double solde;
+	double taux3Mois;//taux pour les 3 premiers mois du compte
+	double taux;//taux pour les mois suivant les 3 premiers mois
+
 public :
-    ecompte(string eml = "vide" ,string nt = "vide" ,double tm =0,double t =0);//constructeur par parametre par defaut
-    ecompte(const ecompte & E);//constructeur par copie
+	ecompte(string eml = "vide" ,string nt = "vide" ,double s=50, double tm =0,double t =0);//constructeur par parametre par defaut
+	ecompte(const ecompte & E);//constructeur par copie
 
 
-    ~ecompte();//destructeur
-    //virtual void Afficher();
+	~ecompte();//destructeur
+	void AfficherSolde(ostream & out);
+	void Retrait();
 
-    ecompte &CreerCompte();
+	ecompte &CreerCompte();
 };
 
 #endif
