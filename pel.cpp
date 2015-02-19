8c221ba35290a80f27e158df49a67135a4184a01
stag <stag@ldnr-formation>
2015-02-19 15:41:09 +0100

    maj du jour

--- pel.cpp
+++ pel.cpp
@@ -14,10 +14,18 @@ tauxEmprunt=monPel.tauxEmprunt;
 
 Pel Pel::CreerCompte()
 {
+int ouverture=0;
+int ecriture=0;
+
 cout<<"Saisir le montant mensuel de versement: ";
 cin >>this->versementMensuel;
 cout<<"Saisir le taux d'emprunt initial: ";
 cin >>this->tauxEmprunt;
+this->soldePel=0;
+
+Client::PEL=true;//???
+
+this->EcritureFichier();
 return *this;
 }
 
@@ -29,7 +37,18 @@ cout<<"Quel montant voulez vous verser? ";
 cin >>mtt;
 
 monPel.soldePel=monPel.soldePel+mtt;
-/**** METTRE A JOUR LE SOLDE COMPTE COURANT!! ****/
+
 }
+
+
+void Pel::EcritureFichier()const
+{
+ofstream monFichier;
+monFichier.open("Pel.txt",ofstream::app);
+monFichier<<this->versementMensuel<<";"<<this->tauxEmprunt<<";"<<this->soldePel<<";"<<endl;
+monFichier.close();
+}
+
+
 
 
